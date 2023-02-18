f2b0f98412fe2fd82307b50a7500edccd8949e6c
NakaokaRei <reideeplearning@gmail.com>
2023-02-18 22:46:09 +0900

    Initial Commit

---
+++ TrackpadAir/TrackpadAirApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackpadAirApp.swift
+//  TrackpadAir
+//
+//  Created by NakaokaRei on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct TrackpadAirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
