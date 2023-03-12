44e852df0ad15320342583eea4ebe802cf65ba88
NakaokaRei <reideeplearning@gmail.com>
2023-03-12 21:40:22 +0900

    fix setting

--- TrackpadAir/View/Settings/SettingsView.swift
+++ TrackpadAir/View/Settings/SettingsView.swift
@@ -7,14 +7,49 @@
 
 import SwiftUI
 
-import Foundation
-import SwiftUI
-
 struct SettingsView: View {
+    @State private var selectedTabIndex = 0
 
     var body: some View {
-        Text("Comming soon")
-            .padding(20)
-            .frame(width: 500, height: 375)
+        TabView(selection: $selectedTabIndex) {
+            GeneralSettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("General")
+                }
+                .tag(0)
+            NotificationsSettingsView()
+                .tabItem {
+                    Image(systemName: "bell")
+                    Text("Notifications")
+                }
+                .tag(1)
+            PrivacySettingsView()
+                .tabItem {
+                    Image(systemName: "lock")
+                    Text("Privacy")
+                }
+                .tag(2)
+        }
+        .padding(20)
+        .frame(width: 500, height: 375)
+    }
+}
+
+struct GeneralSettingsView: View {
+    var body: some View {
+        Text("General Settings")
+    }
+}
+
+struct NotificationsSettingsView: View {
+    var body: some View {
+        Text("Notifications Settings")
+    }
+}
+
+struct PrivacySettingsView: View {
+    var body: some View {
+        Text("Privacy Settings")
     }
 }
